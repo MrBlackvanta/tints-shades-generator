0db30d5116c255a5a80edb0fe8ba8120f2460347
Abdelrhman Ahmed <49564335+MrBlackvanta@users.noreply.github.com>
2023-04-27 14:34:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-3
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />14
-  </React.StrictMode>6
-);57
-2
+    <App />
+  </React.StrictMode>
+);
